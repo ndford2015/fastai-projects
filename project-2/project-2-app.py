32a6a29735c869803245fe7fd88b7bb8c101a03c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-10 03:06:42 +0000

    [MODIFY] update model with better accuracy

--- project-2/project-2-app.py
+++ project-2/project-2-app.py
@@ -4,22 +4,24 @@ from fastai.metrics import error_rate
 from PIL import Image
 import requests
 from io import BytesIO
+from starlette.responses import JSONResponse
 
 def get_bytes(url):
-    response = requests.get(url)
-    return Image.open(BytesIO(response.content))
-
+    return requests.get(url)
+    
+ 
 app = FastAPI()
 learn_path = Path('/home/jupyter/projects/project-2')
 learner = load_learner(learn_path)
 
 @app.get("/classify-food-url")
 async def readUrl(url: str = ''):
-    img = await get_bytes(url)
-    _,_,losses = learner.predict(img)
+    img_bytes = get_bytes(url)
+    fastai_img = open_image(BytesIO(img_bytes.content))
+    _,_,losses = learner.predict(fastai_img)
     return JSONResponse({
         "predictions": sorted(
-            zip(cat_learner.data.classes, losses),
+            zip(learner.data.classes, map(float,losses)),
             key=lambda p: p[1],
             reverse=True
             )
