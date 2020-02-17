a4acf2aabb48e7071c479c6c6c6d3506fcdbe893
Nate Ford <ndford2015@gmail.com>
2020-02-16 20:53:33 -0600

    [MODIFY] attempt to load databunch/models but having issues with path conversion between linux/windows

--- project-3/classify/classify-ingredients.py
+++ project-3/classify/classify-ingredients.py
@@ -1,25 +1,18 @@
 import mysql.connector
 from fastai.text import *
+from pathlib import *
+import os 
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
+  password="com4tornightStand",
   database="recipes",
   auth_plugin="mysql_native_password"
 )
 
-print(mydb)
-
-db_cursor = mydb.cursor()
-
-db_cursor.execute("SHOW TABLES")
-for table in db_cursor:
-	print(table)
-
-## The following lines are all I should need to get my model going at this point
- 
-path = Path('/home/jupyter/fastai-projects/project-3/train'); path
+path = "..\\train"
+path = path.replace(os.altsep, os.sep)
 
 data_lm = load_data(path, 'data_lm_export.pkl')
 data_clas = load_data(path, 'data_clas_export.pkl', bs=16)
@@ -27,3 +20,21 @@ data_clas = load_data(path, 'data_clas_export.pkl', bs=16)
 learn = text_classifier_learner(data_clas, AWD_LSTM, drop_mult=0.5)
 learn.load_encoder('ft_enc')
 learn.load('stage-1')
+
+query = ("SELECT orig_value FROM ingredients "
+         "order by rand() limit 10")
+bad_chars = [',', ';', '(',')', '.']
+
+db_cursor = mydb.cursor()
+
+db_cursor.execute(query)
+
+for val in db_cursor:
+  for bad_char in bad_chars:
+    val = val.replace(bad_char, '')
+  print(val)
+  split_ingr = val.split()
+  for ingr_class in split_ingr:
+    print('val: {0}, class: {1}'.format(ingr_class, learn.predict(ingr_class)))
+
+ 
